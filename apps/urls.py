dd042ff069ad322cdea7ea3e887d07671cff8f58
Izzat <masterfamily23@gmail.com>
2025-02-20 22:14:35 +0500

    Update urls.py

--- apps/urls.py
+++ apps/urls.py
@@ -9,18 +9,11 @@ from apps.views import FilmView, TodoFormView, DelTodoView, TodoCreateView, Cars
 
 urlpatterns = [
     # path("", HomeListView.as_view()),
-    path('film', FilmView.as_view(), name='film-home'),
-    path('cars', CarsView.as_view(), name='cars-home'),
-    path('todo/create', TodoCreateView.as_view(), name='todo-create'),
-    path('todo/list', TodoFormView.as_view(), name='todo_list'),
-    path('tododel', DelTodoView.as_view(), name='todo_del'),
     path("register", RegisterFormView.as_view(), name='register'),
     path("home", HomePageView.as_view(), name='home'),
     path('income', IncomeFormView.as_view(), name='income'),
-    # path('logout', LogoutFormView.as_view(), name='logout'),
     path("", LoginFormView.as_view(), name='login-html'),
     path("Home/list", HomeFormView.as_view(), name='user'),
-    # path("sahifa/list", SahifaFormView.as_view(), name='bosh_sahifa'),
-    # path('logout', logout_view, name='logout'),
+    
 
 ]
